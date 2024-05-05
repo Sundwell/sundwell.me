b93e2ba0590551a6f1bd115ddcf08bc8e6343e8f
Sundwell <lyamtsev.s2017@gmail.com>
2024-05-05 18:53:30 +0200

    Added primevue into project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import './assets/css/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
+app.use(PrimeVue)
